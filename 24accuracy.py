5da4e7e47decd7fa9b9b1af03478d4ffebbc934e
ckung5 <ckung9397@gmail.com>
2024-01-31 15:25:24 -0800

    new

--- 24accuracy.py
+++ 24accuracy.py
@@ -3,17 +3,17 @@
 def performance(tp, fp, tn, fn):
 
 	if (tp + fp) > 0: precision = tp / (tp + fp)
-	else: 			precision = 0
+	else: 		precision = 0
 	
 	if (tp + fn) > 0: recall = tp / (tp + fn)
-	else: 			recall = 0
+	else: 		recall = 0
 	
 	if (precision + recall) > 0: 
-		  f1_score = 2 * (precision * recall) / (precision + recall)
+		f1_score = 2 * (precision * recall) / (precision + recall)
 	else: f1_score = 0
 	
 	if (tp + fp + tn + fn) > 0: 
-		  accuracy = (tp + tn) / (tp + fp + tn + fn)
+		accuracy = (tp + tn) / (tp + fp + tn + fn)
 	else: accuracy = 0
 	
 	return accuracy, f1_score
