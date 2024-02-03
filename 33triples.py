c82554db8cd1132d698616beb0461344440b78af
ckung5 <ckung9397@gmail.com>
2024-02-02 16:03:55 -0800

    new

--- 33triples.py
+++ 33triples.py
@@ -3,5 +3,5 @@
 for a in range(1, 101):
 	for b in range(a+1, 101):
 		for c in range(b+1, 101):
-			if a**2 + b**2 == c**2:
+			if a**2 + b**2 == c**2: 
 				print(a, b, c, sep=',')
--- 35nchoosek.py
+++ 35nchoosek.py
@@ -5,10 +5,8 @@ def factorial(n):
 	else: 				 return n * (factorial(n - 1))
 	
 def n_choose_k(n, k):
-	if k < 0 or k > n: 
-		return 0
-	else: 
-		return factorial(n) // (factorial(k) * factorial(n - k))
+	if k < 0 or k > n: return 0
+	else: 			   return factorial(n) // (factorial(k) * factorial(n - k))
 	
 print(n_choose_k(5, 2))
 print(n_choose_k(9, 4))
