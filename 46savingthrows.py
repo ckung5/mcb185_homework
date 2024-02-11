162cd21de2c5585462683018930c8c90135651e6
ckung5 <ckung9397@gmail.com>
2024-02-11 11:47:18 -0800

    new

--- 46savingthrows.py
+++ 46savingthrows.py
@@ -17,6 +17,8 @@ dis_5_successes = 0
 dis_10_successes = 0
 dis_15_successes = 0
 
+print('DC', 'norm', 'adv', 'dis', sep='\t')
+
 for i in range(rolls):
 	d1 = random.randint(1, 20)
 	d2 = random.randint(1, 20)
@@ -36,12 +38,9 @@ for i in range(rolls):
 	if d1 >= dc_10 and d2 >= dc_10: dis_10_successes += 1
 	if d1 >= dc_15 and d2 >= dc_15: dis_15_successes += 1
 	
-print(norm_5_successes / rolls)
-print(norm_10_successes / rolls)
-print(norm_15_successes / rolls)
-print(adv_5_successes / rolls)
-print(adv_10_successes / rolls)
-print(adv_15_successes / rolls)
-print(dis_5_successes / rolls)
-print(dis_10_successes / rolls)
-print(dis_15_successes / rolls)
+print(dc_5, norm_5_successes / rolls, adv_5_successes / rolls, 
+	dis_5_successes/rolls, sep='\t')
+print(dc_10, norm_10_successes / rolls, adv_10_successes / rolls, 
+	dis_10_successes / rolls, sep='\t')
+print(dc_15, norm_15_successes / rolls, adv_15_successes / rolls, 
+	dis_15_successes / rolls, sep='\t')
