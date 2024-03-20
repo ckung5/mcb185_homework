0828a224039baf43293b863be557298f7d844a6f
ckung5 <ckung9397@gmail.com>
2024-03-21 00:18:43 +0900

    new

--- 74genefinder.py
+++ 74genefinder.py
@@ -4,9 +4,7 @@ import mcb185
 import dogma
 import sys
 
-path = sys.argv[1]
-min_orf_len = int(sys.argv[2])
-
+# Function used to perform six-frame translation of DNA sequence
 def six_frame_translation(dna):
 	frames = []
 	rev_dna = dogma.revcomp(dna)
@@ -17,25 +15,12 @@ def six_frame_translation(dna):
 		# Reverse frames
 		frames.append(dogma.translate(rev_dna[i:]))		
 	return frames
-
-# Function to convert amino acids indices to nucleotide indices
-def aas_to_nts(aas, frame, strand, seq):
-	nts = []
-	for i in aas:
-		if strand == '+':
-			nt_start = frame * 3 + i * 3
-			nt_end = nt_start + min_orf_len * 3
-			nts.append((nt_start, nt_end))
-		else:
-			nt_end = len(seq) - (frame * 3 + i * 3)
-			nt_start = nt_end - min_orf_len * 3
-			nts.append((nt_start, nt_end))
-	return nts
 	
+# Function used to find genes within the DNA sequence based on requirements and
+# determines the specific coordinates of those genes
 def gene_finder(seq, min_orf_len):
-	genes = {}
-	proteins = []
-	
+	genes = {} # Dictionary to store gene coordinates
+	proteins = [] # List to store translated proteins
 	translated_seq = six_frame_translation(seq)
 	
 	for frame in range(3):
@@ -45,29 +30,37 @@ def gene_finder(seq, min_orf_len):
 			else:
 				seq_index = frame + 3
 				
+			# Split translated sequence into individual amino acid sequences
+			# using stop codon
 			for i, aas in enumerate(translated_seq[seq_index].split('*')):
 				m_index = aas.find('M')
 				if m_index != -1 and len(aas[m_index:]) >= min_orf_len:
-					nts = aas_to_nts([m_index], frame, strand, seq)
+					nt_start = frame * 3 + m_index * 3
+					nt_end = nt_start + min_orf_len * 3
+					if strand == '-':
+						nt_end = len(seq) - nt_start
+						nt_start = nt_end - min_orf_len * 3
 					gene_num = f'gene{len(genes) + 1}'
-					genes[gene_num] = nts[0] + (strand,)
+					genes[gene_num] = (nt_start, nt_end, strand)
 					proteins.append(aas)
 					
 	return genes, proteins
 	
-def print_gff(gene_coords, gene_num):
+def generate_gff(gene_coords, gene_num):
 	gff_lines = []
-	for gene_type, gene_info in zip(gene_coords.keys(), gene_coords.values()):
+	for gene_type, gene_info in gene_coords.items():
 		start, end, strand = gene_info
-		attributes = f'ID={gene_num}'
-		gff_lines.append(f'E.coli\tputative_gene\tCDS\t{start}\t{end}\t.\t{strand}\t.\t{attributes}')
+		gff_lines.append(
+			f'E.coli\tputative_gene\tCDS\t{start}\t{end}\t.\t'
+			f'{strand}\t.\tID={gene_num}')
 	return '\n'.join(gff_lines)
 
-ecoli_genome = ''
-for defline, seq in mcb185.read_fasta(path):
-	ecoli_genome += seq
-	
-gene_coord, defline = gene_finder(ecoli_genome, min_orf_len)
-gff_content = print_gff(gene_coord, gene_num='gene')
+path = sys.argv[1]
+min_orf_len = int(sys.argv[2])
 
+for defline, seq in mcb185.read_fasta(path):
+	ecoli_genome = ''.join(seq)
+	gene_coord, defline = gene_finder(ecoli_genome, min_orf_len)
+	gff_content = generate_gff(gene_coord, f'gene')
+	
 print(gff_content)
